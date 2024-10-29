0d031ac16203b3a92d8a9de8a7992c14b6249873
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-10-29 18:57:36 -0400

    Get All implementation end to end

--- src/main/java/net/josephvt/pokemonreview/api/controllers/PokemonController.java
+++ src/main/java/net/josephvt/pokemonreview/api/controllers/PokemonController.java
@@ -23,12 +23,9 @@ public class PokemonController {
     }
 
     @GetMapping("pokemon")
-    public ResponseEntity<List<Pokemon>> getPokemons() {
-        List<Pokemon> pokemons = new ArrayList<>();
-        pokemons.add(new Pokemon(1, "Squirtle", "Water"));
-        pokemons.add(new Pokemon(2, "Pikachu", "Electric"));
-        pokemons.add(new Pokemon(3, "Charmander", "Fire"));
-        return ResponseEntity.ok(pokemons);
+    public ResponseEntity<List<PokemonDto>> getPokemons() {
+        List<PokemonDto> pokemonDtos = pokemonService.getAllPokemons();
+        return ResponseEntity.ok(pokemonDtos);
     }
 
     @GetMapping("pokemon/{id}")
--- src/main/java/net/josephvt/pokemonreview/api/service/PokemonService.java
+++ src/main/java/net/josephvt/pokemonreview/api/service/PokemonService.java
@@ -2,6 +2,9 @@ package net.josephvt.pokemonreview.api.service;
 
 import net.josephvt.pokemonreview.api.dto.PokemonDto;
 
+import java.util.List;
+
 public interface PokemonService {
     PokemonDto createPokemon(PokemonDto pokemonDto);
+    List<PokemonDto> getAllPokemons();
 }
