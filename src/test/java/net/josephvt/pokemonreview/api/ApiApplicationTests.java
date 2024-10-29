6792ae2aaa630081e7d0201236a70c640747f65d
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-10-29 12:37:20 -0400

    Spring Boot project: initial commit from generated project

---
+++ src/test/java/net/josephvt/pokemonreview/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package net.josephvt.pokemonreview.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
