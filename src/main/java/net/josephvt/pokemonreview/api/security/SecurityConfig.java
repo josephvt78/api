3c91095924cb1c8bd3decb6626414f3236a7e593
Joseph Varghese <joseph.varghese.t@gmail.com>
2024-10-31 05:32:35 -0400

    UserDetailService, UserDetails and PasswordEncoder

--- src/main/java/net/josephvt/pokemonreview/api/security/SecurityConfig.java
+++ src/main/java/net/josephvt/pokemonreview/api/security/SecurityConfig.java
@@ -2,8 +2,15 @@ package net.josephvt.pokemonreview.api.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -12,9 +19,24 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(httpSecurityCsrfConfigurer -> {}).authorizeHttpRequests(authorize -> authorize
+        http.csrf(httpSecurityCsrfConfigurer -> {}).authorizeHttpRequests(authorize ->
+                        authorize.requestMatchers(HttpMethod.GET).permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(httpSecurityHttpBasicConfigurer -> {});
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails admin = User.builder().username("admin").password("password").roles("ADMIN").build();
+        UserDetails user = User.builder().username("user").password("password").roles("USER").build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
